339449ded25cb95fe6158164e4df5655a4477eb9
Abdulaziz <abdulazizisa579@gmail.com>
2023-07-14 14:22:29 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -30,7 +30,7 @@ if (n > 0)
 
 {
 
-printf("%d is positive\n", n);
+printf("%d is positive\n" , n);
 
 }
 
@@ -38,7 +38,7 @@ if (n < 0)
 
 {
 
-printf("%d is negative\n", n);
+printf("%d is negative\n" , n);
 
 }
 
@@ -46,7 +46,7 @@ if (n == 0)
 
 {
 
-printf("%d is zero\n", n);
+printf("%d is zero\n" , n);
 
 }
 
