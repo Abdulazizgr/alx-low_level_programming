90e94155214b5ae1f641392a509f91424456c7d5
Abdulaziz <abdulazizisa579@gmail.com>
2023-08-06 08:37:20 -0700

    fix done

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,7 +9,6 @@
 int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
-
-        printf("%d\n", argc - 1);
-	return (0);
+printf("%d\n", argc - 1);
+return (0);
 }
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - prints all arguments it receives
  * @argc: number of arguments
@@ -10,12 +9,10 @@
  */
 int main(int argc, char *argv[])
 {
-        int i;
-
-        for (i = 0; i < argc; i++)
-        {
-                printf("%s\n", argv[i]);
-        }
-
-        return (0);
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
