5ca1a35937decd630b6893a94ad031a0e2fa0fd7
Abdulaziz <abdulazizisa579@gmail.com>
2023-08-07 01:41:06 -0700

    0x09. C - Static libraries Task and explanation

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+int _isalpha(int c)
+
+{
+
+    return isalpha(c);
+
+}
+
+
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+
+#include <string.h>
+
+
+int _putchar(char c)
+
+{
+
+    return putchar(c);
+
+}
+
+
