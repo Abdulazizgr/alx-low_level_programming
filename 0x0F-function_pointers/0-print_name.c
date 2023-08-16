b7eb4b5d751d820567186c5eced995e65f238ce2
Abdulaziz <abdulazizisa579@gmail.com>
2023-08-16 11:31:01 -0700

    function

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -7,10 +7,10 @@
 * Return: nothing
 **/
 void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
+{
+	if (name == NULL || f == NULL)
+		return;
 
 
 		f(name);
-	}
+}
