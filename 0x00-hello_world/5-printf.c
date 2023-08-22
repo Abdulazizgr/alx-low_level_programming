052c1a9bb23ef3d8198e3d00ec8b812ba19229f2
Abdulazizgr <abdulazizisa579@gmail.com>
2023-08-22 06:26:08 -0700

    printf cd git add . clear git commit -m printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main-Entry point
+ *
+ * return: Always 0 (succsess)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
