567307a72005d442be58771d7e150d030087bab5
Abdulazizgr <abdulazizisa579@gmail.com>
2023-08-22 07:03:04 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,8 +3,8 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- * 
  */
+
 int main(void)
 {
 		printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -14,3 +14,5 @@ int main(void)
 		printf("Size of a float: %d byte(s)\n", sizeof(float));
 		return (0);
 }
+
+
