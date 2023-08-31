669f9ca7f808d94eb3fa6e8e380070c021975159
Abdulazizgr <abdulazizisa579@gmail.com>
2023-08-31 13:03:08 -0700

    bit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,19 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num = 0;
-        unsigned int k;
+    unsigned int num = 0;
+    unsigned int k;
 
-        if (b == NULL)
-                return (0);
+    if (b == NULL)
+        return (0);
 
-        for (k = 0; b[k] != '\0'; k++)
-        {
-                if (b[k] != '0' && b[k] != '1')
-                        return (0);
+    for (k = 0; b[k] != '\0'; k++)
+    {
+        if (b[k] != '0' && b[k] != '1')
+            return (0);
 
-                num = num * 2 + (b[k] - '0');
-        }
+        num = num * 2 + (b[k] - '0');
+    }
 
-        return (num);
+    return (num);
 }
